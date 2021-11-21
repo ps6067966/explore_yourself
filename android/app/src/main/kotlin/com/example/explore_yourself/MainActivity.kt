d3b282ff0f58773bff86c1b07ca0a8d3c0fd95dc
Pratap Singh <ps6067966@gmail.com>
2021-11-21 16:07:26 +0530

    new cool project

---
+++ android/app/src/main/kotlin/com/example/explore_yourself/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.explore_yourself
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/views/onboarding/onboarding_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingScreen extends StatefulWidget {
+  const OnBoardingScreen({Key? key}) : super(key: key);
+
+  @override
+  _OnBoardingScreenState createState() => _OnBoardingScreenState();
+}
+
+class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+          child: Column(
+        children: [
+          Expanded(
+            child: PageView.builder(itemBuilder: (ctx, index) {
+              return Container();
+            }),
+          )
+        ],
+      )),
+    );
+  }
+}
